53ec27a54c9fc1937b765a4b9c832f5754a04b61
Randall Bollig <gitkraken@bacn.randallbollig.com>
2018-06-26 21:23:15 -0400

    Add configuration examples

--- src/config-example.php
+++ src/config-example.php
@@ -1,13 +1,13 @@
 <?php
 
-$server             = '';
-$username           = '';
-$password           = '';
-$box                = '';
-$storage_directory  = '';
-$pgp_key_id         = '';
-$database_directory = '';
-$batch_limit        = 0; // Maximum number of messages per run (0 for unlimited)
+$server             = 'secure.emailsrvr.com:993/imap/ssl';
+$username           = 'user@domain.tld';
+$password           = 'ReDaCt3d';
+$box                = 'INBOX';
+$storage_directory  = 'storage';
+$pgp_key_id         = 'B8E85CDD14BA632FB442EFD2E690C606000702E9';
+$database_directory = 'data';
+$batch_limit        = 100; // Maximum number of messages per run (0 for unlimited)
 
 
 ini_set("memory_limit", "256M");
